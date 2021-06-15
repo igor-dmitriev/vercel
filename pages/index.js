bce0fa873bda8147339d79b1aeb0b4218b9e0320
Ihor Dmitriiev <i.dmitriiev@spd-ukraine.com>
2021-06-15 20:24:44 +0300

    change

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home() {
       <main className={styles.main}>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Vercel Play
         </h1>
 
         <p className={styles.description}>
